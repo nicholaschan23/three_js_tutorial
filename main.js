38afdf95deac05915673c015b73b9742806bb4ea
Nick Chan <nick.chan@utexas.edu>
2024-04-05 14:46:02 -0500

    sun earth moon

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
+import { AxisGridHelper } from './AxisGridHelper.js';
+import GUI from 'lil-gui';
 
 // creating the scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 50, 0);
 camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
@@ -14,26 +16,69 @@ document.body.appendChild(renderer.domElement);
 // an array of objects whose rotation to update
 const objects = [];
 
-// use just one sphere for everything
+// reusable sphere geometry
 const radius = 1;
-const widthSegments = 6;
-const heightSegments = 6;
+const widthSegments = 12;
+const heightSegments = 12;
 const sphereGeometry = new THREE.SphereGeometry(
     radius, widthSegments, heightSegments);
 
+// empty scene for sun
+const solarSystem = new THREE.Object3D();
+scene.add(solarSystem);
+objects.push(solarSystem);
+
+// sun
 const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
 const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
 sunMesh.scale.set(5, 5, 5);  // make the sun large
-scene.add(sunMesh);
+solarSystem.add(sunMesh);
 objects.push(sunMesh);
+
+// empty scene for earth
+const earthOrbit = new THREE.Object3D();
+earthOrbit.position.x = 10;
+solarSystem.add(earthOrbit);
+objects.push(earthOrbit);
+
+// earth
+const earthMaterial = new THREE.MeshPhongMaterial({ color: 0x2233FF, emissive: 0x112244 });
+const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+earthOrbit.add(earthMesh);
+objects.push(earthMesh);
+
+// empty scene for moon
+const moonOrbit = new THREE.Object3D();
+moonOrbit.position.x = 2;
+earthOrbit.add(moonOrbit);
+
+// moon
+const moonMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, emissive: 0x222222 });
+const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+moonMesh.scale.set(.5, .5, .5);
+moonOrbit.add(moonMesh);
+objects.push(moonMesh);
 
 // point of light in the center of the scene
 {
     const color = 0xFFFFFF;
-    const intensity = 3;
+    const intensity = 200;
     const light = new THREE.PointLight(color, intensity);
     scene.add(light);
 }
+
+const gui = new GUI();
+function makeAxisGrid(node, label, units) {
+    const helper = new AxisGridHelper(node, units);
+    gui.add(helper, 'visible').name(label);
+}
+
+makeAxisGrid(solarSystem, 'solarSystem', 25);
+makeAxisGrid(sunMesh, 'sunMesh');
+makeAxisGrid(earthOrbit, 'earthOrbit');
+makeAxisGrid(earthMesh, 'earthMesh');
+makeAxisGrid(moonOrbit, 'moonOrbit');
+makeAxisGrid(moonMesh, 'moonMesh');
 
 // rendering the scene
 function animate() {
